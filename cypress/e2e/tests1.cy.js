3d278133cb2864f92f53d3eae2c75cc6010507ab
rostik-307 <rkonchenko@cic.es>
2024-08-29 10:32:02 +0200

    fix tests

--- cypress/e2e/tests1.cy.js
+++ cypress/e2e/tests1.cy.js
@@ -1,34 +1,34 @@
 // https://on.cypress.io/api
 
-describe('Looks a ver si funciona', () => {
+describe('Page up?', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('a', 'Listado')
+    cy.contains('a', 'Autores')
   })
 
-  it('checks un botón a ver si lo manda to another URL', () => {
+  it('Check edit redirect', () => {
     cy.visit('/')
     cy.get('table').within(() => {
       cy.get('button').contains('Editar').click()
     })
-    cy.url().should('include', '/users')
+    cy.url().should('include', '/author')
   })
 
-  it('checks for a specific element on the about page', () => {
+  it('Check secondary page', () => {
     cy.visit('/about')
     cy.contains('h1', 'Lorem Ipsum')
   })
 
-  it('fills out and submits a formulario', () => {
-    cy.visit('/create')
+  it('Fill out a form and check for creation confirmation', () => {
+    cy.visit('/create-author')
     cy.get('[id^=nombre]').type('aaa')
     cy.get('[id^=apellidos]').type('123')
     cy.get('[id^=anno]').type('1999')
     cy.get('form').submit()
-    cy.contains('div', 'User aaa 123 created.')
+    cy.contains('div', 'Author aaa 123 created.')
   })
 
-  it('deletes an element and checks for the deleted notification', () => {
+  it('Delete an element and check for the deleted notification', () => {
     cy.visit('/')
     cy.get('table').within(() => {
       cy.get('button').contains('Borrar').click()
--- src/views/AuthorsView.vue
+++ src/views/AuthorsView.vue
@@ -68,7 +68,7 @@ export default {
         async handleAuthorDeleted(author) {
             const toast = useToast();
             try {
-                await axios.delete(`/api/persona/${author.id}`);
+                await axios.delete(`/api/autor/${author.id}`);
                 this.authors = this.authors.filter(u => u.id !== author.id);
                 if (this.selectedAuthor && this.selectedAuthor.id === author.id) {
                     this.selectedAuthor = null;
